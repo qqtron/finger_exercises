a2d5866fd1624f949457925b395ad0a06d011805
Pablo Sanchez <pablosanchez@MBP-de-Pablo.home>
2020-04-05 20:53:14 +0200

    added bosque program

---
+++ bosque.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  4 21:22:05 2020
+
+@author: pablosanchez
+"""
+
+
+text = input("Please write your name: ")
+
+if text == "My name is Pablo":
+    print("Hola Pablo")
+else:
+    print("hola, {}".format(text))
